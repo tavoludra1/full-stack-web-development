8a8d437d50590f694bf7716f5f18704f89065965
GustavoP <tavoludra@hotmail.com>
2022-06-26 22:46:32 +0000

    Fixed HMR

--- svelte.config.js
+++ svelte.config.js
@@ -13,7 +13,7 @@ const config = {
 			server: {
 				hmr: {
 					clientPort: process.env.HMR_HOST ? 443 : 3000,
-					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".lenght) : "localhost"
+					host: process.env.HMR_HOST ? process.env.HMR_HOST.substring("https://".length) : "localhost"
 
 				}
 			}
